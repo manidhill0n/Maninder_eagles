5d017817acf01f03745948ac4f8e5265e50c79fb
manidhill0n <manidhillon1989@gmail.com>
2016-11-27 20:27:07 +0100

    class_work_4

---
+++ cw_4.R
@@ -0,0 +1,11 @@
+band_3<-raster("H:\Eagle_subjects\Sem1\geostatistics\r_practice\R-GIS-tutorial-master\figure\gmap31.tif",band=3)
+allbands<-brick("")
+
+plotRGB(allbands,3.2.1)
+plotRGB(allbands)
+
+
+
+
+
+
